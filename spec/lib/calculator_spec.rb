9a76f5df24c2bd323b3f466c9979757563200bce
shubham <shubhamsingh1295@yahoo.in>
2024-05-18 21:44:19 +0530

    feat: handle single input string

--- spec/lib/calculator_spec.rb
+++ spec/lib/calculator_spec.rb
@@ -6,5 +6,9 @@ RSpec.describe Calculator, type: :model do
     it "returns 0 for an empty string" do
       expect(Calculator.add("")).to eq(0)
     end
+
+    it "returns the number itself for a single number" do
+      expect(Calculator.add("1")).to eq(1)
+    end
   end
 end
